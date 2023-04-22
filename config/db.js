1a07f49ed0c69e901bb2cabb56e8117fdd4a50b8
Ravi Lour <ravijaat2917@gmail.com>
2023-04-22 14:03:54 +0530

    adding strict querry to the mongodb conn Code

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import colors from "colors";
+
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
