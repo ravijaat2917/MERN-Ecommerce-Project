2aad3883824e6433aa69338cb5401bcd28e77178
Ravi Lour <ravijaat2917@gmail.com>
2023-04-22 16:36:31 +0530

    removing console logs

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      // `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
     console.log(`Errro in Mongodb ${error}`.bgRed.white);
